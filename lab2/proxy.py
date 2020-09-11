fa80f3308efc3749ef5e37daf5208cad66358fa9
gasc <mayeul.gasc@orange.fr>
2020-09-11 11:01:40 +0200

    adding python version

--- lab2/proxy.py
+++ lab2/proxy.py
@@ -13,8 +13,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
     conn, addr = server_socket.accept()
     with conn:
         print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(data)
+        data = conn.recv(1024)
+
+        print(data.decode("utf-8"))
