1797a1bbb58ad8fc0ccd55dec20710caf0e9d85c
gasc <mayeul.gasc@orange.fr>
2020-09-11 11:56:09 +0200

    work on lab 2

--- lab2/proxy.py
+++ lab2/proxy.py
@@ -1,18 +1,50 @@
 #!/usr/bin/env python3
 
 import socket
+import requests
 
-HOST = '127.0.0.1'  # Localhost
-PORT = 8080         # HTTP proxy port
+HOST = '127.0.0.1'      # Localhost
+INTERNAL_PORT = 8080    # HTTP proxy internal port
+EXTERNAL_PORT = 8081    # Port for server connection
 
-print("Init test.")
+print("Init proxy...")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+    client_socket.bind((HOST, INTERNAL_PORT))
+    client_socket.listen()
+    print("Proxy ready and waiting...")
+
+    conn, addr = client_socket.accept()
+    with conn:
+        print("Connected by", addr)
+        print("Receiving data...")
+        data = conn.recv(1024)
+        print(data.decode("utf-8"))
+
+        request = data.decode("utf-8").split("\n")
+        header = request[0].split(' ')
+        method = header[0]
+
+        if method == "GET":
+            url = header[1]
+            server = url.split('/')[2]
+            server_ip = socket.gethostbyname(server)
+            protocol = header[2]
+            print("Interception of a GET method requesting the URL: ", url)
+            print("Destination server: ", server, " (", server_ip, ")")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind((HOST, PORT))
+    server_socket.bind((HOST, EXTERNAL_PORT))
+
+    server_socket.connect((server_ip, 80))
+    sent = server_socket.send(bytes(request[0], "utf-8"))
+
     server_socket.listen()
     conn, addr = server_socket.accept()
     with conn:
-        print('Connected by', addr)
+        print("Connected by", addr)
+        print("Receiving data...")
         data = conn.recv(1024)
-
         print(data.decode("utf-8"))
+
+    pass
